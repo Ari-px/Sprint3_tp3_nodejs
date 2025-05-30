dcc54eec8c263eac29a0b88cddda6102cd66f44b
AriPanque <arianaespeche@gmail.com>
2025-05-30 00:42:22 -0300

    Eliminar reponseView.mjs

--- views/reponseView.mjs
+++
@@ -1,9 +0,0 @@
-export function renderizarSuperheroe(superheroe)
-{
-    return JSON.stringify(superheroe,null,2);
-}
-
-export function renderizarListaSuperheroes(superheroes)
-{
-    return JSON.stringify(superheroes,null,2);
-}
