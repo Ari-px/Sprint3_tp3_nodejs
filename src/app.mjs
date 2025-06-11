17fdc2573d6aa1951f0bb727540ad0fc0b7bc958
Ariana de Jesús Espeche <arianaespeche@gmail.com>
2025-06-11 00:06:34 -0300

    app.mjs

---
+++ src/app.mjs
@@ -0,0 +1,21 @@
+import express from 'express';
+import {connectDB} from './config/dbconfig.mjs';
+import superHeroRoutes from './routes/superHeroRoutes.mjs';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+connectDB();
+
+app.use('/api',superHeroRoutes);
+
+app.use((req,res) => {
+    res.status(404).send({mensaje:"Ruta no encontrada"});
+});
+
+app.listen(PORT,()=>
+{
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
