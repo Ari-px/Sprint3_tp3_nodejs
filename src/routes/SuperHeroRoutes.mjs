a085a0273b28628f201737226754afe0a63c0db3
AriPanque <116276066+Ari-px@users.noreply.github.com>
2025-06-11 00:39:40 -0300

    Update SuperHeroRoutes.mjs

--- src/routes/SuperHeroRoutes.mjs
+++ src/routes/SuperHeroRoutes.mjs
@@ -1,17 +1,17 @@
 import express from "express";
 import {
-  buscarSuperheroesPorAtributoController,
-  obtenerSuperheroePorIdController,
-  obtenerSuperheroesMayoresDe30Controller,
-  obtenerTodosLosSuperheroesController,
-} from "../controllers/superheroesController.mjs";
+  buscarSuperheroesPorAtributoControllers,
+  obtenerSuperheroePorIdControllers,
+  obtenerSuperheroesMayoresDe30Controllers,
+  obtenerTodosLosSuperheroesControllers,
+} from "../controllers/superheroesControllers.mjs";
 
 const router = express.Router();
 
-router.get("/heroes/mayores-30", obtenerSuperheroesMayoresDe30Controller);
-router.get("/heroes", obtenerTodosLosSuperheroesController);
-router.get("/heroes/:id", obtenerSuperheroePorIdController);
-router.get("/heroes/:atributo/:valor", buscarSuperheroesPorAtributoController);
+router.get("/heroes/mayores-30", obtenerSuperheroesMayoresDe30Controllers);
+router.get("/heroes", obtenerTodosLosSuperheroesControllers);
+router.get("/heroes/:id", obtenerSuperheroePorIdControllers);
+router.get("/heroes/:atributo/:valor", buscarSuperheroesPorAtributoControllers);
 
 
 export default router;
