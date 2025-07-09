9eae7b41be3e33265fd1eae0e3974ebd7a574af4
Ariana Espeche <arianaespeche@gmail.com>
2025-07-09 17:21:15 -0300

    agrega app.mjs

--- app.mjs
+++ app.mjs
@@ -1,21 +1,28 @@
 import express from 'express';
-import {connectDB} from './dbconfig.mjs';
+import {connectDB} from './dbConfig.mjs';
 import superHeroRoutes from './rutaSuperheroes.mjs';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Middleware para parsear JSON
 app.use(express.json());
 
-connectDB();
+// Conexion a DB
+await connectDB();
 
-app.use('/api',superHeroRoutes);
+// Rutas
+app.use("/api", superHeroRoutes);
 
+// Manejo de errores para rutas no encontradas
 app.use((req,res) => {
-    res.status(404).send({mensaje:"Ruta no encontrada"});
-});
+  res.status(400).send({mensaje: 'Ruta no encontrada'})
+})
 
-app.listen(PORT,()=>
-{
-    console.log(`Servidor escuchando en el puerto ${PORT}`);
+// Levantar el servidor
+app.listen(PORT, () => {
+  console.log("##########################");
+  console.log("######## API REST ########");
+  console.log("##########################");
+  console.log(`http://localhost:${PORT}/`);
 });
